8f0078f3dd33982d52afb7f2b0575ba27271d1fc
Mina Arsham <miena.arsham@gmail.com>
2026-01-31 03:48:38 +0100

    changed te tf template

--- terraform-project/aws-s3-provisioner/skeleton/main.tf
+++ terraform-project/aws-s3-provisioner/skeleton/main.tf
@@ -64,16 +64,27 @@ resource "aws_s3_bucket_versioning" "main" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   bucket = aws_s3_bucket.main.id
 
+{% if values.encryption_type == "AES256" %}
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "{% if values.encryption_type == 'AES256' %}AES256{% else %}aws:kms{% endif %}"
+      sse_algorithm = "AES256"
     }
-
-  {% if values.encryption_type != "AES256" %}
+  }
+{% elif values.encryption_type == "aws:kms" %}
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
     bucket_key_enabled = true
-  {% endif %}
-}
-
+  }
+{% elif values.encryption_type == "aws:kms:dsse" %}
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms:dsse"
+    }
+    bucket_key_enabled = true
+  }
+{% endif %}
 }
 
 # Block Public Access
