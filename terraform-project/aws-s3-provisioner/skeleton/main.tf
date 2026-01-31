f0a7b70251f18c438d571b730d25196c760ba017
Mina Arsham <miena.arsham@gmail.com>
2026-01-31 15:05:37 +0100

    clean up

--- terraform-project/aws-s3-provisioner/skeleton/main.tf
+++ terraform-project/aws-s3-provisioner/skeleton/main.tf
@@ -101,79 +101,12 @@ resource "aws_s3_bucket_public_access_block" "main" {
   ignore_public_acls      = {% if values.block_public_access %}true{% else %}false{% endif %}
   restrict_public_buckets = {% if values.block_public_access %}true{% else %}false{% endif %}
 }
-{%- if values.enable_access_logging %}
 
-# Access Logging Bucket
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "${local.bucket_name}-logs"
-  
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.bucket_name}-logs"
-      Type = "AccessLogs"
-    }
-  )
-}
-
-resource "aws_s3_bucket_logging" "main" {
-  bucket = aws_s3_bucket.main.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
-}
-{%- endif %}
-{%- if values.enable_lifecycle_policy %}
-
-# Lifecycle Policy
-resource "aws_s3_bucket_lifecycle_configuration" "main" {
-  bucket = aws_s3_bucket.main.id
-
-  rule {
-    id     = "transition-and-expiration"
-    status = "Enabled"
-
-    transition {
-      days          = ${{ values.lifecycle_transition_days }}
-      storage_class = "STANDARD_IA"
-    }
-{%- if values.lifecycle_expiration_days > 0 %}
-
-    expiration {
-      days = ${{ values.lifecycle_expiration_days }}
-    }
-{%- endif %}
-
-    noncurrent_version_transition {
-      noncurrent_days = ${{ values.lifecycle_transition_days + 30 }}
-      storage_class   = "GLACIER"
-    }
-{%- if values.lifecycle_expiration_days > 0 %}
-
-    noncurrent_version_expiration {
-      noncurrent_days = ${{ values.lifecycle_expiration_days + 30 }}
-    }
-{%- endif %}
-  }
-}
-{%- endif %}
-{%- if values.enable_cors %}
-
-# CORS Configuration
-resource "aws_s3_bucket_cors_configuration" "main" {
-  bucket = aws_s3_bucket.main.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD", "PUT", "POST"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-}
-{%- endif %}
 {%- if values.enable_static_website %}
 
+#############################
+# Advanced Features (Optional)
+#############################
 # Static Website Hosting
 resource "aws_s3_bucket_website_configuration" "main" {
   bucket = aws_s3_bucket.main.id
@@ -187,30 +120,26 @@ resource "aws_s3_bucket_website_configuration" "main" {
   }
 }
 {%- endif %}
+
+################################
+# S3 Bucket Policy (Optional)
+################################
 {%- if values.bucket_policy_type != "none" %}
 
-# Bucket Policy
 resource "aws_s3_bucket_policy" "main" {
   bucket = aws_s3_bucket.main.id
-{%- if values.bucket_policy_type == "custom" %}
-
-  policy = <<POLICY
-${{ values.custom_policy_json }}
-POLICY
-{%- else %}
-
   policy = data.aws_iam_policy_document.bucket_policy.json
-{%- endif %}
-  
   depends_on = [aws_s3_bucket_public_access_block.main]
 }
-{%- if values.bucket_policy_type != "custom" %}
 
-# Data source for generating bucket policies based on type
 data "aws_iam_policy_document" "bucket_policy" {
+
+################################
+# Read-Only (Public GetObject)
+################################
+
 {%- if values.bucket_policy_type == "read_only" %}
 
-  # Public Read-Only Access
   statement {
     sid    = "PublicReadGetObject"
     effect = "Allow"
@@ -226,80 +155,13 @@ data "aws_iam_policy_document" "bucket_policy" {
     ]
   }
 {%- endif %}
-{%- if values.bucket_policy_type == "cloudfront_oac" and values.cloudfront_distribution_arn %}
 
-  # CloudFront Origin Access Control (OAC)
-  statement {
-    sid    = "AllowCloudFrontServicePrincipal"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "${aws_s3_bucket.main.arn}/*"
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = ["${{ values.cloudfront_distribution_arn }}"]
-    }
-  }
-{%- endif %}
-{%- if values.bucket_policy_type == "vpc_endpoint" and values.vpc_endpoint_id %}
+################################
+# Cross-Account Access
+################################
 
-  # VPC Endpoint Restriction - Allow
-  statement {
-    sid    = "AllowVPCEndpointAccess"
-    effect = "Allow"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:ListBucket"
-    ]
-    resources = [
-      aws_s3_bucket.main.arn,
-      "${aws_s3_bucket.main.arn}/*"
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceVpce"
-      values   = ["${{ values.vpc_endpoint_id }}"]
-    }
-  }
-
-  # VPC Endpoint Restriction - Deny
-  statement {
-    sid    = "DenyNonVPCEndpointAccess"
-    effect = "Deny"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:*"
-    ]
-    resources = [
-      aws_s3_bucket.main.arn,
-      "${aws_s3_bucket.main.arn}/*"
-    ]
-    condition {
-      test     = "StringNotEquals"
-      variable = "aws:SourceVpce"
-      values   = ["${{ values.vpc_endpoint_id }}"]
-    }
-  }
-{%- endif %}
 {%- if values.bucket_policy_type == "specific_accounts" and values.allowed_aws_accounts and (values.allowed_aws_accounts | length) > 0 %}
 
-  # Cross-Account Access
   statement {
     sid    = "AllowCrossAccountAccess"
     effect = "Allow"
@@ -325,51 +187,5 @@ data "aws_iam_policy_document" "bucket_policy" {
   }
 {%- endif %}
 
-  # Require SSL/TLS (Deny non-HTTPS) - Applied to all policy types except custom
-  statement {
-    sid    = "DenyInsecureTransport"
-    effect = "Deny"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:*"
-    ]
-    resources = [
-      aws_s3_bucket.main.arn,
-      "${aws_s3_bucket.main.arn}/*"
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
-  }
-{%- if values.bucket_policy_type == "require_ssl" %}
-
-  # Require SSL/TLS - Allow statement
-  statement {
-    sid    = "AllowSSLRequestsOnly"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:*"
-    ]
-    resources = [
-      aws_s3_bucket.main.arn,
-      "${aws_s3_bucket.main.arn}/*"
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["true"]
-    }
-  }
-{%- endif %}
 }
-{%- endif %}
 {%- endif %}
